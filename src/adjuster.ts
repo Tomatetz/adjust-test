1693356b1b59713110e55eb671ebc8f4a37c9435
Aleksei Ganikhin <alexei@upvest.co>
2020-04-10 18:13:29 +0200

    Add last added date memo

--- src/adjuster.ts
+++ src/adjuster.ts
@@ -2,12 +2,11 @@ import { SecondsStack, HTTPResponse } from "./model";
 
 class Adjuster {
   secondsStack: SecondsStack;
-  requestInAction: boolean; // Flag to avoid double fetching on window focus
+  lastAdded: Date;
+  requestIsPending: boolean; // Flag to avoid double fetching on window focus
   windowIsBlur: boolean;
   constructor() {
     this.secondsStack = new Array();
-    this.requestInAction = false;
-    this.windowIsBlur = false;
     window.addEventListener("focus", () => {
       this.windowIsBlur = false;
       this.secondsStack.length && this.fetchData(this.secondsStack[0]);
@@ -18,22 +17,31 @@ class Adjuster {
   }
   updateSecondsStack = () => {
     const { fetchData, secondsStack } = this;
-    const seconds: number = new Date().getSeconds();
-    ~secondsStack.indexOf(seconds) // Bitwise NOT, ~-1 = 0
-      ? this.updateOutput(`Repeated seconds alert (${seconds})`, "repeat")
-      : (secondsStack.push(seconds), fetchData(seconds)); // Put seconds to stack, attempt to fetch
+    const currentDate = new Date();
+    const seconds: number = currentDate.getSeconds();
+    if (
+      !this.lastAdded || // Check for initial call or
+      seconds !== this.lastAdded.getSeconds() || // Not the same second or
+      currentDate.getTime() - this.lastAdded.getTime() > 1000 // More than a second has passed
+    ) {
+      this.lastAdded = currentDate;
+      secondsStack.push(seconds);
+      fetchData(seconds);
+    } else {
+      this.updateOutput(`Repeated seconds alert (${seconds})`, "repeat");
+    }
   };
   fetchData = async (seconds: number) => {
-    if (!this.requestInAction && !this.windowIsBlur) {
-      this.requestInAction = true;
+    if (!this.requestIsPending && !this.windowIsBlur) {
+      this.requestIsPending = true;
       let result = await this.makeRequest(JSON.stringify({ seconds }));
       this.updateOutput(`id: ${result.id}, seconds: ${seconds}`, "response");
-      this.requestInAction = false;
+      this.requestIsPending = false;
       this.secondsStack.shift(); // Remove utilized seconds from stack
       if (this.secondsStack.length) this.fetchData(this.secondsStack[0]); // Fetch next
     }
   };
-  makeRequest(request: string) {
+  makeRequest = (request: string) => {
     return new Promise<HTTPResponse>((resolve) => {
       let xhr = new XMLHttpRequest();
       // xhr.open("POST", "https://reqres.in/api/users?delay=4"); // Request with delay to debug window blur
@@ -43,12 +51,12 @@ class Adjuster {
       };
       xhr.send(request);
     });
-  }
-  updateOutput(value: string, className: string) {
+  };
+  updateOutput = (value: string, className: string) => {
     document.getElementById(
       "output"
-    ).innerHTML += `<li class=${className}>${value}</li>`; // Log output factory
-  }
+    ).innerHTML += `<li class=${className}>${value}</li>`; // Output log factory
+  };
 }
 const adjaster = new Adjuster();
 
